5e622fd089dace9fc252eeaeec7bdc93dd8b6c5a
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-04-11 01:16:13 +0100

    Update W12MForm.tsx
    Added fields to the form

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 import W12MHeader from './W12MHeader';
+import Fields from './fields';
 
 const W12MForm = () => {
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
+
+<Fields label="Species" type="text" name="txtSpecies" />
+<Fields label="Planet" type="text" name="txtPlanet" />
+<Fields label="Numberof Beings" type="text" name="txtNoOfBeings" />
+<Fields label="What is 2+2" type="select" name="sel2+2" />
+<Fields label="species" type="textarea" name="txtReason"/>
+<Fields label="Submit" type="button" name="btnSubmit">
+	</Fields>		
+		{/* REST OF FORM GOES HERE */}
 		</section>
 	);
 };
